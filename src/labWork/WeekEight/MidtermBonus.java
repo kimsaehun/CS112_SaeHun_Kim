bbf01d05af7683eb38efbea8382cfe0fbb21f9de
Sae Hun Kim <guest25369@gmail.com>
2013-10-15 21:28:08 -0700

    Sae Hun Kim

--- src/labWork/WeekEight/MidtermBonus.java
+++ src/labWork/WeekEight/MidtermBonus.java
@@ -8,14 +8,14 @@ package labWork.WeekEight;
 import java.util.Scanner;
 
 public class MidtermBonus {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
-		int M = 1;
-		int F = 1;
-		for (; M <= N; ++M) {
-			F *= M;
-		}
-		System.out.print(F);
-	}
+        public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                int N = scan.nextInt();
+                int M = 1;
+                int F = 1;
+                for (; M != N; ++M) {
+                        F *= M;
+                }
+                System.out.print(F);
+        }
 }
