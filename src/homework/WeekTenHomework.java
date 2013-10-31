a68d0992a0f55936af564af62a742fc836ad668e
Sae Hun Kim <guest25369@gmail.com>
2013-10-30 20:21:10 -0700

    Sae Hun Kim

---
+++ src/homework/WeekTenHomework.java
@@ -0,0 +1,86 @@
+/*
+ * Sae Hun Kim
+ * Cs 112
+ * Week ten homework
+ */
+
+package homework;
+
+import java.util.ArrayList;
+
+public class WeekTenHomework {
+	private int value, throwValue, numOfThrows = 0;
+	private static int oneValue;
+	private ArrayList<Integer> history = new ArrayList<Integer>();
+
+	public WeekTenHomework() {// bounces should = 1 for a default constructor
+		Throw(1);
+	}
+
+	public WeekTenHomework(int bounces) {// constructor. this would be the
+											// Dice(int
+		// bounces)
+		Throw(bounces);
+	}
+
+	public int getThrowValue() {
+		return throwValue;
+	}
+
+	public static void OneThrow() {
+		oneValue = (int) ((Math.random() * 6) + 1);
+	}
+
+	public int getOneValue() {
+		return oneValue;
+	}
+
+	public int Throw() {
+		numOfThrows++;
+		value = (int) ((Math.random() * 6) + 1);
+		history.add(value);
+		return value;
+	}
+
+	public int Throw(int bounces) {// get the average number from the dice being
+		// rolled bounces times
+		numOfThrows++;
+		int totalValueRolled = 0;
+		int numRolled = 0;
+		for (int i = 0; i < bounces; i++) {
+			Throw();
+			totalValueRolled += Value();
+			numRolled++;
+		}
+		throwValue = totalValueRolled / numRolled;
+		history.add(throwValue);
+		return throwValue;
+	}
+
+	public int Throw(int dices, int bounces) {
+		numOfThrows++;
+		int returnValue = 0;
+		for (int i = 0; i < dices; i++) {
+			returnValue += Throw(bounces);
+		}
+		returnValue /= dices;
+		history.add(returnValue);
+		return returnValue;
+	}
+
+	public int Value() {
+		return value;
+	}
+
+	// methods added for the homework
+	public int getNumOfThrows() {// returns the number of throws done
+		return numOfThrows;
+	}
+
+	public void getHistory() {// prints out all the values from throwing the
+								// dice in a single line
+		for (int i = 0; i < history.size(); i++) {
+			System.out.print(history.get(i) + " ");
+		}
+	}
+}
